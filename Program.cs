52715a89fe1e88bed11f0c61406fe4e2053b4c65
JessPageJHC <17285@jhc.school.nz>
2022-03-03 17:37:22 +1300

    Basic game is working, no graphics or mistakes limit yet

--- Program.cs
+++ Program.cs
@@ -38,6 +38,20 @@ class Program
             }
         }
 
+        public bool Contains(char c)
+        {
+            bool contains = false;
+            foreach (Character character in Word)
+            {
+                if (character.Value == Char.ToUpper(c))
+                {
+                    contains = true;
+                }
+            }
+
+            return contains;
+        }
+
         public void Show(char c)
         {
             foreach (Character character in Word)
@@ -55,6 +69,20 @@ class Program
             {
                 character.Hidden = false;
             }
+        }
+
+        public bool Finished()
+        {
+            bool finished = true;
+            foreach (Character character in Word)
+            {
+                if (character.Hidden)
+                {
+                    finished = false;
+                }
+            }
+            
+            return finished;
         }
 
         public override string ToString()
@@ -82,6 +110,7 @@ class Program
         // Initialize
         Clear();
         ForegroundColor = ConsoleColor.White;
+        CursorVisible = true;
         Random random = new Random();
 
         // Get files from "categories" folder
@@ -116,13 +145,84 @@ class Program
             }
         }
 
+        // Create a new puzzle with a random word from the selected category
         string category = Directory.GetCurrentDirectory() + @"\categories\" + files[selection].Name;
         List<string> words = File.ReadAllLines(category).ToList();
+        string word = words[random.Next(words.Count)];
+        Puzzle puzzle = new Puzzle(word);
 
-        Puzzle puzzle = new Puzzle(words[random.Next(words.Count)]);
+        CursorVisible = false;
+        bool win = true;
+        int mistakes = 0;
+        List<char> incorrectLetters = new List<char>();
+        while (true)
+        {
+            Clear();
+            WriteLine(puzzle.ToString());
+            
+            char attempt = ' ';
+            switch (ReadKey(true).Key)
+            {
+                case ConsoleKey.A: { attempt = 'a'; break; }
+                case ConsoleKey.B: { attempt = 'b'; break; }
+                case ConsoleKey.C: { attempt = 'c'; break; }
+                case ConsoleKey.D: { attempt = 'd'; break; }
+                case ConsoleKey.E: { attempt = 'e'; break; }
+                case ConsoleKey.F: { attempt = 'f'; break; }
+                case ConsoleKey.G: { attempt = 'g'; break; }
+                case ConsoleKey.H: { attempt = 'h'; break; }
+                case ConsoleKey.I: { attempt = 'i'; break; }
+                case ConsoleKey.J: { attempt = 'j'; break; }
+                case ConsoleKey.K: { attempt = 'k'; break; }
+                case ConsoleKey.L: { attempt = 'l'; break; }
+                case ConsoleKey.M: { attempt = 'm'; break; }
+                case ConsoleKey.N: { attempt = 'n'; break; }
+                case ConsoleKey.O: { attempt = 'o'; break; }
+                case ConsoleKey.P: { attempt = 'p'; break; }
+                case ConsoleKey.Q: { attempt = 'q'; break; }
+                case ConsoleKey.R: { attempt = 'r'; break; }
+                case ConsoleKey.S: { attempt = 's'; break; }
+                case ConsoleKey.T: { attempt = 't'; break; }
+                case ConsoleKey.U: { attempt = 'u'; break; }
+                case ConsoleKey.V: { attempt = 'v'; break; }
+                case ConsoleKey.W: { attempt = 'w'; break; }
+                case ConsoleKey.X: { attempt = 'x'; break; }
+                case ConsoleKey.Y: { attempt = 'y'; break; }
+                case ConsoleKey.Z: { attempt = 'z'; break; }
+            }
 
-        Clear();
-        puzzle.ShowAll();
-        WriteLine(puzzle.ToString());
+            if (puzzle.Contains(attempt))
+            {
+                puzzle.Show(attempt);
+            }
+            else if (!puzzle.Contains(attempt) && !incorrectLetters.Contains(attempt))
+            {
+                incorrectLetters.Add(attempt);
+                mistakes++;
+            }
+
+            if (puzzle.Finished())
+            {
+                win = true;
+                break;
+            }
+        }
+
+        if (win)
+        {
+            Clear();
+            WriteLine(" == WELL DONE! ==");
+            WriteLine("\n You got the word:");
+            WriteLine("  {0}", word);
+            if (mistakes == 0)
+            {
+                WriteLine(" with no mistakes!");
+            }
+            else
+            {
+                WriteLine(" with {0} mistakes!", mistakes);
+            }
+            WriteLine("\n Play again? (y/n)");
+        }
     }
 }
